cc62a12b92105c07b2681314b3b4e1d87b7136b4
huangtao <kris.ht@alibabapictures.com>
2017-07-21 12:26:06 +0800

    实现https 重定向问题

--- src/main/java/com/znz/util/Constants.java
+++ src/main/java/com/znz/util/Constants.java
@@ -28,7 +28,7 @@ public class Constants {
     public static  final String  ADMIN_FLAG = "isAdmin";
 
     public static  final String  WATERMARK_PARAM = "watermarkParam";
-    public static  final String INDEX_PAGE ="https://www.tyulan.com/";
+    public static  final String INDEX_PAGE ="/";
 
 
     public static void main(String[] args) {
