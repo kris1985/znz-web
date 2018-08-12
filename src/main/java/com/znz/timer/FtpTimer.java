99228e7ec5f72c9d7927b6e66c8de8c0acd65f92
huangtao <kris.ht@alibabapictures.com>
2018-08-13 01:35:50 +0800

    删除FTP

---
+++ src/main/java/com/znz/timer/FtpTimer.java
@@ -0,0 +1,39 @@
+package com.znz.timer;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class FtpTimer {
+
+    @Scheduled(cron="0 0/10 * * * ?")
+    private  void reset() throws IOException {
+        log.info("----------start jobs FtpTimer------------");
+        List<String> list = Lists.newArrayList(".","..","a","cfj","css","data","flash","group","gxjy","images","img","include","m","plus","special","templets","uploads");
+        FTPClient ftpClient = new FTPClient();
+        String parentPath = "/www";
+        ftpClient.connect("cd2012.gotoip.net");
+        ftpClient.login("vh480580","6730011asA");
+        FTPFile[] ftpFiles = ftpClient.listDirectories(parentPath);
+        for (FTPFile ftpFile:ftpFiles){
+            String fileName = ftpFile.getName();
+            if(list.contains(fileName)){
+                continue;
+            }
+           boolean success =  ftpClient.removeDirectory(parentPath+"/"+fileName);
+           log.info("delete Directory success:{}",success);
+        }
+    }
+/*
+    public static void main(String[] args) throws IOException {
+        reset();
+    }*/
+}
